2f846a93d3aabdf5266bac6412313e3e2251090b
Hugo Souza <hugosouzalmeida@gmail.com>
2025-04-23 01:21:30 -0300

    Update aumento

--- Atividades/src/main/java/lista001/Aumento.java
+++ Atividades/src/main/java/lista001/Aumento.java
@@ -7,32 +7,40 @@ public class Aumento {
     private float porcentagem;
 
     public String getNome() {
+
         return this.nome;
     }
 
     public void setNome(String nome) {
+
         this.nome = nome;
     }
 
     public float getSalario() {
+
         return this.salario;
     }
 
     public void setSalario(float salario) {
+
         this.salario = salario;
     }
 
     public float getPorcentagem() {
+
         return this.porcentagem;
     }
 
     public void setPorcentagem(float porcentagem) {
+
         this.porcentagem = porcentagem;
     }
     public float PorcentagemAumento() {
+
         return this.salario * (this.porcentagem / 100);
     }
     public float calcularAumento() {
+
         return this.PorcentagemAumento() + salario;
     }
 
