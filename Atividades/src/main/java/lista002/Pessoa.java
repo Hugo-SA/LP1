ea67a0b7ce44dbf7504067f0cb21a16a3f12aad3
Hugo Souza <hugosouzalmeida@gmail.com>
2025-05-07 23:00:36 -0300

    Update
    Update classe Pessoa e PessoaTest

--- Atividades/src/main/java/lista002/Pessoa.java
+++ Atividades/src/main/java/lista002/Pessoa.java
@@ -1,15 +1,33 @@
 package lista002;
 
 public class Pessoa {
-    private int idade;
+    private String nome;
+    private float idade;
     private float altura;
     private float peso;
 
-    public int getIdade() {
+    public Pessoa(){
+        this.idade = 0;
+        this.altura = 0.00f;
+        this.peso = 0.00f;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getIdade() {
         return idade;
     }
 
-    public void setIdade(int idade) {
+    public void setIdade(float idade) {
+        if (idade < 0){
+            throw new IllegalArgumentException("idade invalida");
+        }
         this.idade = idade;
     }
 
@@ -18,6 +36,9 @@ public class Pessoa {
     }
 
     public void setAltura(float altura) {
+        if(altura < 0){
+            throw new IllegalArgumentException("altura invalida");
+        }
         this.altura = altura;
     }
 
@@ -26,6 +47,31 @@ public class Pessoa {
     }
 
     public void setPeso(float peso) {
+        if(peso < 0){
+            throw new IllegalArgumentException("peso invalido");
+        }
         this.peso = peso;
+    }
+
+    public float envelhecer(){
+        return idade ++;
+    }
+    public void crescer(){
+        if(idade >= 21){
+            throw new IllegalArgumentException("pessoa nao cresce mais");
+        }
+         altura += 0.01f;
+    }
+    public float ganharPeso(float kg){
+        if(kg < 0){
+            throw new IllegalArgumentException("nao se pode ganhar massa negativa");
+        }
+        return peso += kg;
+    }
+    public float perderPeso(float kg){
+        if(kg < 0){
+            throw new IllegalArgumentException("nao se pode perder massa negativa");
+        }
+        return peso-= kg;
     }
 }
--- Atividades/src/test/java/lista002Test/PessoaTest.java
+++ Atividades/src/test/java/lista002Test/PessoaTest.java
@@ -1,4 +1,97 @@
 package lista002Test;
+import lista002.Pessoa;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class PessoaTest {
+    Pessoa pessoa;
+
+    @BeforeEach
+    public void setUp() {
+        pessoa = new Pessoa();
+    }
+
+    @Test
+    public void deveTestarEnvelhecer(){
+        pessoa.setIdade(30);
+        pessoa.envelhecer();
+        assertEquals(31, pessoa.getIdade());
+    }
+    @Test
+    public void deveTestarCrescer(){
+        pessoa.setIdade(20);
+        pessoa.setAltura(1.75f);
+        pessoa.crescer();
+        assertEquals(1.76f, pessoa.getAltura());
+    }
+    @Test
+    public void deveTestarGanharPeso(){
+        pessoa.setPeso(50.0f);
+        pessoa.ganharPeso(20.0f);
+        assertEquals(70.0f, pessoa.getPeso());
+    }
+
+
+    @Test
+    public void deveTestarIdadeErrada(){
+        try{
+            pessoa.setIdade(-1);
+            fail();
+        }
+        catch (IllegalArgumentException e){
+            assertEquals("idade invalida", e.getMessage());
+        }
+    }
+    @Test
+    public void deveTestarAlturaErrada(){
+        try{
+            pessoa.setAltura(-0.1f);
+        }
+        catch (IllegalArgumentException e){
+            assertEquals("altura invalida", e.getMessage());
+        }
+    }
+    @Test
+    public void deveTestarPesoErrado(){
+        try{
+            pessoa.setPeso(-0.1f);
+        }
+        catch (IllegalArgumentException e){
+            assertEquals("peso invalido", e.getMessage());
+        }
+    }
+    @Test
+    public void deveTestarCrescerErrado(){
+        try{
+            pessoa.setIdade(21);
+            pessoa.crescer();
+            fail();
+        }
+        catch (IllegalArgumentException e){
+            assertEquals("pessoa nao cresce mais", e.getMessage());
+        }
+    }
+    @Test
+    public void deveTestarGanharMassaNegativa(){
+        try{
+            pessoa.ganharPeso(-1);
+            fail();
+        }
+        catch (IllegalArgumentException e){
+            assertEquals("nao se pode ganhar massa negativa", e.getMessage());
+        }
+    }@Test
+    public void deveTestarPerderMassaNegativa(){
+        try{
+            pessoa.perderPeso(-1);
+            fail();
+        }
+        catch (IllegalArgumentException e){
+            assertEquals("nao se pode perder massa negativa", e.getMessage());
+        }
+    }
+
 }
